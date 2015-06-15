9bdbb6dae047e474d602919ba5654981e14597c9
Alec Arme & Adam McFadden <student@epicodus.com>
2015-06-15 15:39:31 -0700

    add wonderful styling and beautiful colors.

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 var attr = DS.attr;
 
 export default DS.Model.extend({
-  tweet: attr('string'),
-  user: DS.belongsTo('user')
+  name: attr('string'),
+  posts: DS.hasMany('post')
 });
--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -1,15 +1,70 @@
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+var posts = [
+  {
+    id: 1,
+    tweet: 'That\'s no moon...',
+    user: 1
+  },
+  {
+    id: 2,
+    tweet: 'It\'s a trap!',
+    user: 2
+  },
+  {
+    id: 3,
+    tweet: 'I find your lack of faith disturbing.',
+    user: 3
+  }
+];
+
+var users = [
+  {
+    id: 1,
+    name: 'b3n K3n0b1',
+    posts: [1]
+  },
+  {
+    id: 2,
+    name: 'ADMIRAL AKBAR',
+    posts: [2]
+  },
+  {
+    id: 3,
+    name: 'DontCallMeAnakin',
+    posts: [3]
+  }
+];
+
 module.exports = function(app) {
   var express = require('express');
   var postsRouter = express.Router();
 
   postsRouter.get('/', function(req, res) {
     res.send({
-      'posts': [
-        {
-          id: 1,
-          tweet: 'ol Ben'
-        }
-      ]
+      'posts': posts,
+      'users': users
     });
   });
 
@@ -18,12 +73,12 @@ module.exports = function(app) {
   });
 
   postsRouter.get('/:id', function(req, res) {
-    res.send({
-      'posts': {
-        id: req.params.id
-      }
+      res.send({
+        'post': posts.find(function(post) {
+          return post.id == req.params.id
+        })
+      });
     });
-  });
 
   postsRouter.put('/:id', function(req, res) {
     res.send({
